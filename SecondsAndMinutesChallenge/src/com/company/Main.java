44af0b582a8e390ba0d07e82a904eb974fa88304
SergiOn <onishhenko@gmail.com>
2018-07-24 09:40:35 +0300

    #complete-java-masterclass # Section 5, Lecture 38 # Bonus Challenge Solution

--- SecondsAndMinutesChallenge/src/com/company/Main.java
+++ SecondsAndMinutesChallenge/src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class Main {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
     public static void main(String[] args) {
 
         String time = getDurationString(2, 10);
@@ -16,26 +18,32 @@ public class Main {
         time = getDurationString(70);
         System.out.println("Time " + time);
 
+        time = getDurationString(3700);
+        System.out.println("Time " + time);
+
+        time = getDurationString(-41);
+        System.out.println("Time " + time);
+
     }
 
     private static String getDurationString(int minutes, int seconds) {
         if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
 
-        String fullHours = hours < 10 ? "0" + hours : "" + hours;
-        String fullMinutes = remainingMinutes < 10 ? "0" + remainingMinutes : "" + remainingMinutes;
-        String fullSeconds = seconds < 10 ? "0" + seconds : "" + seconds;
+        String hoursString = hours < 10 ? "0" + hours + "h" : "" + hours + "h";
+        String minutesString = remainingMinutes < 10 ? "0" + remainingMinutes + "m" : "" + remainingMinutes  + "m";
+        String secondsString = seconds < 10 ? "0" + seconds  + "s" : "" + seconds + "s";
 
-        return fullHours + "h " + fullMinutes + "m " + fullSeconds + "s";
+        return hoursString + " " + minutesString + " " + secondsString;
     }
 
     private static String getDurationString(int seconds) {
         if (seconds < 0) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int minutes = seconds / 60;
