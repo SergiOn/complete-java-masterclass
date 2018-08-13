5513c26349f58b080ab67436c191bc52e53cd130
SergiOn <onishhenko@gmail.com>
2018-08-14 01:28:39 +0300

    #complete-java-masterclass # Section 7, Lecture 54 # Classes Part 1

---
+++ Classes/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porshe = new Car();
+        Car holden = new Car();
+        porshe.model = "Carrera";
+    }
+}
