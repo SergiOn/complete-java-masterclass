1a5699a7c38cdc3d2c91e86b95f35fdabb34f197
SergiOn <onishhenko@gmail.com>
2018-08-14 22:43:45 +0300

    #complete-java-masterclass # Section 7, Lecture 57 # Constructors - Part 2 (+Challenge Exercise)

--- Constructors/src/com/company/Account.java
+++ Constructors/src/com/company/Account.java
@@ -35,6 +35,13 @@ public class Account {
         this.customerPhoneNumber = customerPhoneNumber;
     }
 
+    public Account(String customerName, String customerEmailAddress, String customerPhoneNumber) {
+        this("99999", 100.55, customerName, customerEmailAddress, customerPhoneNumber);
+//        this.customerName = customerName;
+//        this.customerEmailAddress = customerEmailAddress;
+//        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
     public void deposit(double depositAmount) {
         this.balance += depositAmount;
         System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
