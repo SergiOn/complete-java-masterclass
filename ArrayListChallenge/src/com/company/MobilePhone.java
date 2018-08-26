a278c9220077575d1016e93b56b8cc1ce3271b8e
SergiOn <onishhenko@gmail.com>
2018-08-26 16:08:27 +0300

    #complete-java-masterclass # Section 9, Lecture 88 # Bug Fix for ArrayList Challenge

--- ArrayListChallenge/src/com/company/MobilePhone.java
+++ ArrayListChallenge/src/com/company/MobilePhone.java
@@ -26,6 +26,10 @@ public class MobilePhone {
         if (foundPosition < 0) {
             System.out.println(oldContact.getName() + ", was not found.");
             return false;
+        } else if (findContact(newContact.getName()) != -1) {
+            System.out.println("Contact with name " + newContact.getName() +
+                    " already exist. Update was not successful.");
+            return false;
         }
 
         myContacts.set(foundPosition, newContact);
