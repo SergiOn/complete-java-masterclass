21c5e2bf54603d2d8403dd473bc747619e14d349
SergiOn <onishhenko@gmail.com>
2018-08-30 09:58:12 +0300

    #complete-java-masterclass # Section 11, Lecture 113 # Generics Introduction

---
+++ Generics/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        ArrayList items = new ArrayList();
+//        ArrayList<Integer> items = new ArrayList<Integer>();
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+//        items.add("Tim");
+        items.add(4);
+        items.add(5);
+
+        printDoubled(items);
+
+    }
+
+//    private static void printDoubled(ArrayList n) {
+    private static void printDoubled(ArrayList<Integer> n) {
+//        for (Object i : n) {
+//            System.out.println((Integer) i * 2);
+//        }
+
+//        for (Integer i : n) {
+        for (int i : n) {
+            System.out.println(i * 2);
+        }
+    }
+}
