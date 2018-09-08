32d48e05cd0c573269784cf3af738f5b32a918c8
SergiOn <onishhenko@gmail.com>
2018-09-08 11:32:56 +0300

    #complete-java-masterclass # Section 13, Lecture 139 # Immutable Class Challenge

--- Adventure/src/com/timbuchalka/Main.java
+++ Adventure/src/com/timbuchalka/Main.java
@@ -16,6 +16,16 @@ public class Main {
         //
         // Single letter commands (N, W, S, E, Q) should still be available.
 
+
+        // Here is your challenge and the challenges to work out
+        // what is wrong with the location constructor
+        // that would allow the program to compile and crash at runtime
+        // so when you identify the problem modify the code to fix it
+        // so I just i’ll just say that again your challenge here is to work out
+        // what is wrong with the location constructor that you can see on screen line 14
+        // that will allow the program to compile but crash on run time
+        // when you identify the problem modify the code to fix it.
+
 	    Scanner scanner = new Scanner(System.in);
 
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
