61ee8e3f7e75eb3ea815f9db5353f00e3eddbfdb
SergiOn <onishhenko@gmail.com>
2018-09-09 23:43:50 +0300

    #complete-java-masterclass # Section 16, Lecture 241 # Runnable and Thread

--- Threads/src/com/company/Main.java
+++ Threads/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import static com.company.ThreadColor.ANSI_GREEN;
 import static com.company.ThreadColor.ANSI_PURPLE;
+import static com.company.ThreadColor.ANSI_RED;
 
 public class Main {
 
@@ -9,13 +10,26 @@ public class Main {
         System.out.println(ANSI_PURPLE + "Hello from the main thread");
 
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
+//        anotherThread.run();
 
         new Thread() {
             public void run() {
                 System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
             }
         }.start();
+
+//        Thread myRunnableThread = new Thread(new MyRunnable());
+//        myRunnableThread.start();
+
+        Thread myRunnableThread = new Thread(new MyRunnable() {
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+            }
+        });
+        myRunnableThread.start();
 
         System.out.println(ANSI_PURPLE + "Hello again from the main thread");
 
