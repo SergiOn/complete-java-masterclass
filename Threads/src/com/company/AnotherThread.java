16979061392d942cc0e685893d700ea2f0d0cea9
SergiOn <onishhenko@gmail.com>
2018-09-09 23:22:10 +0300

    #complete-java-masterclass # Section 16, Lecture 240 # Threads

---
+++ Threads/src/com/company/AnotherThread.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import static com.company.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from the another thread");
+    }
+
+}
