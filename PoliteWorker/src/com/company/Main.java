36117a8062a425f63369738c92d303c810aa3f77
SergiOn <onishhenko@gmail.com>
2018-09-10 18:00:55 +0300

    #complete-java-masterclass # Section 16, Lecture 257 # Fair Locks and Live Locks #2

---
+++ PoliteWorker/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    final Worker worker1 = new Worker("Worker 1", true);
+        final Worker worker2 = new Worker("Worker 2", true);
+
+    }
+}
