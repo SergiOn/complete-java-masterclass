ba66fa8ae8775a29922525f07b9c6241e1d93438
SergiOn <onishhenko@gmail.com>
2018-09-10 16:47:48 +0300

    #complete-java-masterclass # Section 16, Lecture 251 # Using Try Finally With Threads

--- ProducerConsumer/src/com/company/Main.java
+++ ProducerConsumer/src/com/company/Main.java
@@ -40,8 +40,11 @@ class MyProducer implements Runnable {
             try {
                 System.out.println(color + "Adding..." + num);
                 bufferLock.lock();
-                buffer.add(num);
-                bufferLock.unlock();
+                try {
+                    buffer.add(num);
+                } finally {
+                    bufferLock.unlock();
+                }
 
                 Thread.sleep(random.nextInt(1000));
             } catch(InterruptedException e) {
@@ -51,8 +54,11 @@ class MyProducer implements Runnable {
 
         System.out.println(color + "Adding EOF and exiting...");
         bufferLock.lock();
-        buffer.add("EOF");
-        bufferLock.unlock();
+        try {
+            buffer.add("EOF");
+        } finally {
+            bufferLock.unlock();
+        }
     }
 }
 
@@ -68,20 +74,49 @@ class MyConsumer implements Runnable {
     }
 
     public void run() {
+
+        int counter = 0;
+
         while(true) {
-            bufferLock.lock();
-            if(buffer.isEmpty()) {
-                bufferLock.unlock();
-                continue;
-            }
-            if(buffer.get(0).equals(Main.EOF)) {
-                System.out.println(color + "Exiting");
-                bufferLock.unlock();
-                break;
+
+//            bufferLock.lock();
+//            try {
+//                if(buffer.isEmpty()) {
+//                    continue;
+//                }
+//                System.out.println(color + "The counter = "+ counter);
+//                counter = 0;
+//
+//                if(buffer.get(0).equals(Main.EOF)) {
+//                    System.out.println(color + "Exiting");
+//                    break;
+//                } else {
+//                    System.out.println(color + "Removed " + buffer.remove(0));
+//                }
+//            } finally {
+//                bufferLock.unlock();
+//            }
+
+            if(bufferLock.tryLock()) {
+                try {
+                    if(buffer.isEmpty()) {
+                        continue;
+                    }
+                    System.out.println(color + "The counter = "+ counter);
+                    counter = 0;
+
+                    if(buffer.get(0).equals(Main.EOF)) {
+                        System.out.println(color + "Exiting");
+                        break;
+                    } else {
+                        System.out.println(color + "Removed " + buffer.remove(0));
+                    }
+                } finally {
+                    bufferLock.unlock();
+                }
             } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
+                counter++;
             }
-            bufferLock.unlock();
         }
     }
 }
