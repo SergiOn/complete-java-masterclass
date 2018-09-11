f8434553ed81e525dbf326ee9ce003f397242e13
SergiOn <onishhenko@gmail.com>
2018-09-11 23:30:01 +0300

    #complete-java-masterclass # Section 17, Lecture 277 # Streams - Intermediate and Terminal Operations

--- Streams/src/com/company/Main.java
+++ Streams/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -28,10 +29,18 @@ public class Main {
         someBingoNumbers
                 .stream()
                 .map(String::toUpperCase)
-                .filter(s -> s.startsWith("G"))
+                .filter(s->s.startsWith("G"))
                 .sorted()
                 .forEach(System.out::println);
 
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "O71");
+        Stream<String> inNumberStream = Stream.of("N40", "N36", "I26", "I17", "I29", "O71");
+        Stream<String> concatStream = Stream.concat(ioNumberStream, inNumberStream);
+        System.out.println("-----------------------");
+        System.out.println(concatStream
+                .distinct()
+                .peek(System.out::println)
+                .count());
 
     }
 }
