96a2803852e5059e8e6d0154cc078b1eff367d72
SergiOn <onishhenko@gmail.com>
2018-09-12 00:35:01 +0300

    #complete-java-masterclass # Section 17, Lecture 280 # Lambda Challenge Part 2

--- LambdaChallenge/src/com/company/Main.java
+++ LambdaChallenge/src/com/company/Main.java
@@ -29,7 +29,7 @@ public class Main {
             }
         };
 
-        Function<String, String> lambdaFunction = s -> {
+        Function<String, String> lambdaFunction =  s -> {
             StringBuilder returnVal = new StringBuilder();
             for (int i = 0; i < s.length(); i++) {
                 if (i % 2 == 1) {
@@ -49,21 +49,52 @@ public class Main {
         String supplierResult = iLoveJava.get();
         System.out.println(supplierResult);
 
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+
+        List<String> firstUpperCaseList = new ArrayList<>();
+//        topNames2015.forEach(name ->
+//                firstUpperCaseList.add(name.substring(0,1).toUpperCase() + name.substring(1)));
+//        firstUpperCaseList.sort((s1, s2) -> s1.compareTo(s2));
+//        firstUpperCaseList.forEach(s -> System.out.println(s));
+//        firstUpperCaseList.sort(String::compareTo);
+//        firstUpperCaseList.forEach(System.out::println);
+
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .sorted(String::compareTo)
+                .forEach(System.out::println);
+
+        long namesBeginningWithA = topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .filter(name -> name.startsWith("A"))
+                .count();
+
+        System.out.println("Number of names beginning with A is: " + namesBeginningWithA);
+
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
+
+
     }
 
     public static String everySecondCharacter(Function<String, String> func, String source) {
         return func.apply(source);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
