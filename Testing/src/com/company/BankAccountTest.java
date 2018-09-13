2cd46d607b636fcbab37c1281a0be4e0ee0ca025
SergiOn <onishhenko@gmail.com>
2018-09-13 23:35:46 +0300

    #complete-java-masterclass # Section 19, Lecture 296 # Parameterized Testing

--- Testing/src/com/company/BankAccountTest.java
+++ Testing/src/com/company/BankAccountTest.java
@@ -40,8 +40,10 @@ public class BankAccountTest {
 
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void withdraw_notBranch() throws Exception {
-        double balance = account.withdraw(600.00, false);
-        assertEquals(400.00, balance, 0);
+//        double balance = account.withdraw(600.00, false);
+//        assertEquals(400.00, balance, 0);
+        account.withdraw(600.00, false);
+        fail("should have thrown an IllegalArgumentException");
     }
 
 //    @org.junit.Test
