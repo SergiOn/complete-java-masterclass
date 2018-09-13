60ae6751a9a9dc9031f8300ba2812f3d3304f266
SergiOn <onishhenko@gmail.com>
2018-09-13 22:55:26 +0300

    #complete-java-masterclass # Section 19, Lecture 294 # Asserts in Junit #1

--- Testing/src/com/company/BankAccountTest.java
+++ Testing/src/com/company/BankAccountTest.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.fail;
 
 public class BankAccountTest {
     @org.junit.Test
     public void deposit() throws Exception {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
     }
 
     @org.junit.Test
@@ -13,8 +17,29 @@ public class BankAccountTest {
         fail("This test has yet to be implemented");
     }
 
+//    @org.junit.Test
+//    public void getBalance() throws Exception {
+//        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+//        account.deposit(200.00, true);
+//        assertEquals(1200.00, account.getBalance(), 0);
+//    }
+
     @org.junit.Test
-    public void getBalance() throws Exception {
+    public void getBalance_deposit() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void getBalance_withdraw() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true() {
         fail("This test has yet to be implemented");
     }
 
