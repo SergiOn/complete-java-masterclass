9810e824aa110fb31c0e733ee74d565e58cb2ff3
SergiOn <onishhenko@gmail.com>
2018-09-16 12:38:34 +0300

    #complete-java-masterclass # Section 20, Lecture 313 # Creating Databases With JDBC in Java

---
+++ TestDB/src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // /Users/serhii/Documents/Web/Training/Java/complete-java-masterclass/TestDB
+
+//        try {
+////            Class.forName("org.sql.JDBC");
+//            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/serhii/Documents/Web/Training/Java/complete-java-masterclass/TestDB/testjava.db");
+//            Statement statement = conn.createStatement();
+//            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+//
+//        } catch (SQLException e) {
+//            System.out.println("Something went wrong: " + e.getMessage());
+//        }
+
+//        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/serhii/Documents/Web/Training/Java/complete-java-masterclass/TestDB/testjava.db");
+//            Statement statement = conn.createStatement()) {
+//
+//            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+//
+//        } catch (SQLException e) {
+//            System.out.println("Something went wrong: " + e.getMessage());
+//        }
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/serhii/Documents/Web/Training/Java/complete-java-masterclass/TestDB/testjava.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+
+            statement.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
+}
