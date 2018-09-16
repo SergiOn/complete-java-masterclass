25afa54c1f339527e18d254fdbf85ba09c32be27
SergiOn <onishhenko@gmail.com>
2018-09-17 01:03:32 +0300

    #complete-java-masterclass # Section 20, Lecture 321 # Result Set Meta Data

--- Music/src/com/company/Main.java
+++ Music/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import com.company.model.Artist;
 import com.company.model.Datasource;
+import com.company.model.SongArtist;
 
 import java.util.List;
 
@@ -25,12 +26,25 @@ public class Main {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
-        List<String> albumsForArtist =
-                datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
+        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
 
         for(String album : albumsForArtist) {
             System.out.println(album);
         }
+
+        List<SongArtist> songArtists = datasource.queryArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_ASC);
+        if(songArtists == null) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for(SongArtist artist : songArtists) {
+            System.out.println("Artist name = " + artist.getArtistName() +
+                " Album name = " + artist.getAlbumName() +
+                " Track = " + artist.getTrack());
+        }
+
+        datasource.querySongsMetadata();
 
         datasource.close();
 
