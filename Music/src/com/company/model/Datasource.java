997e6594034fd5771e399213fcd221d88910f8f0
SergiOn <onishhenko@gmail.com>
2018-09-17 22:48:49 +0300

    #complete-java-masterclass # Section 20, Lecture 328 # Test Insert JDBC Code

--- Music/src/com/company/model/Datasource.java
+++ Music/src/com/company/model/Datasource.java
@@ -387,7 +387,7 @@ public class Datasource {
         }
     }
 
-    private void insertSong(String title, String artist, String album, int track) {
+    public void insertSong(String title, String artist, String album, int track) {
 
         try {
             conn.setAutoCommit(false);
@@ -404,7 +404,7 @@ public class Datasource {
                 throw new SQLException("The song insert failed");
             }
 
-        } catch(SQLException e) {
+        } catch(Exception e) {
             System.out.println("Insert song exception: " + e.getMessage());
             try {
                 System.out.println("Performing rollback");
