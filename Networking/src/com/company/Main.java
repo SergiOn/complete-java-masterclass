79bab75f0271c38627052c3b78119d8c60b3b457
SergiOn <onishhenko@gmail.com>
2018-09-20 00:38:40 +0300

    #complete-java-masterclass # Section 21, Lecture 343 # Alternatives to HTTPUrlConnection

--- Networking/src/com/company/Main.java
+++ Networking/src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            URL url = new URL("http://example.org");
+            URL url = new URL("https://api.flickr.com/services/feeds/photos_public.gne?tags=dogs");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("User-Agent", "Chrome");
@@ -23,6 +23,7 @@ public class Main {
 
             if(responseCode != 200) {
                 System.out.println("Error reading web page");
+                System.out.println(connection.getResponseMessage());
                 return;
             }
 
@@ -37,36 +38,68 @@ public class Main {
 
             inputReader.close();
 
-//            URL url = new URL("http://example.org");
-//            URLConnection urlConnection = url.openConnection();
-//            urlConnection.setDoOutput(true);
-//            urlConnection.connect();
-//
-//            BufferedReader inputStream = new BufferedReader(
-//                    new InputStreamReader(urlConnection.getInputStream()));
-//
-//            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
-//            for(Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
-//                String key = entry.getKey();
-//                List<String> value = entry.getValue();
-//                System.out.println("-----key = " + key);
-//                for(String string: value) {
-//                    System.out.println("value = " + value);
-//                }
-//            }
-//
-//            String line = "";
-//            while(line != null) {
-//                line = inputStream.readLine();
-//                System.out.println(line);
-//            }
-//            inputStream.close();
-
         } catch(MalformedURLException e) {
             System.out.println("Malformed URL: " + e.getMessage());
         } catch(IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
+
+//        try {
+//            URL url = new URL("http://example.org");
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("GET");
+//            connection.setRequestProperty("User-Agent", "Chrome");
+//            connection.setReadTimeout(30000);
+//
+//            int responseCode = connection.getResponseCode();
+//            System.out.println("Response code: " + responseCode);
+//
+//            if(responseCode != 200) {
+//                System.out.println("Error reading web page");
+//                return;
+//            }
+//
+//            BufferedReader inputReader = new BufferedReader(
+//                    new InputStreamReader(connection.getInputStream()));
+//
+//            String line;
+//
+//            while((line = inputReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//
+//            inputReader.close();
+//
+////            URL url = new URL("http://example.org");
+////            URLConnection urlConnection = url.openConnection();
+////            urlConnection.setDoOutput(true);
+////            urlConnection.connect();
+////
+////            BufferedReader inputStream = new BufferedReader(
+////                    new InputStreamReader(urlConnection.getInputStream()));
+////
+////            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+////            for(Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+////                String key = entry.getKey();
+////                List<String> value = entry.getValue();
+////                System.out.println("-----key = " + key);
+////                for(String string: value) {
+////                    System.out.println("value = " + value);
+////                }
+////            }
+////
+////            String line = "";
+////            while(line != null) {
+////                line = inputStream.readLine();
+////                System.out.println(line);
+////            }
+////            inputStream.close();
+//
+//        } catch(MalformedURLException e) {
+//            System.out.println("Malformed URL: " + e.getMessage());
+//        } catch(IOException e) {
+//            System.out.println("IOException: " + e.getMessage());
+//        }
 
 //        try {
 //            URL url = new URL("http://example.org");
